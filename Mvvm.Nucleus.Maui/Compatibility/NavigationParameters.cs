36755295919212cb1c703def0d54d988c6c22a01
Erik Goverde <erik@goverde.me>
2024-02-28 07:17:43 +0100

    Expanded compatibility navigation parameters, as well as nuget values.

--- Mvvm.Nucleus.Maui/Compatibility/NavigationParameters.cs
+++ Mvvm.Nucleus.Maui/Compatibility/NavigationParameters.cs
@@ -2,4 +2,15 @@
 
 public class NavigationParameters : Dictionary<string, object>, INavigationParameters
 {
+    public NavigationParameters() : base()
+    {
+    }
+
+    public NavigationParameters(IDictionary<string, object> dictionary) : base(dictionary)
+    {
+    }
+
+    public NavigationParameters(IEnumerable<KeyValuePair<string, object>> collection) : base(collection)
+    {
+    }
 }
